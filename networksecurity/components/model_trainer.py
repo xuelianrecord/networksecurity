85cf11c67bcb04b6d07bb7445beab404b3fecb7a
xuelianrecord <105611860+xuelianrecord@users.noreply.github.com>
2025-06-15 18:56:41 -0500

    Update model_trainer.py

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -27,11 +27,11 @@ import mlflow
 from urllib.parse import urlparse
 
 import dagshub
-#dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
+#dagshub.init(repo_owner='xxxxxxx', repo_name='networksecurity', mlflow=True)
 
-os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
-os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
-os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"
+os.environ["MLFLOW_TRACKING_URI"]="xxxxx"
+os.environ["MLFLOW_TRACKING_USERNAME"]="xxxxxx"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="xxxxxxx"
 
 
 
